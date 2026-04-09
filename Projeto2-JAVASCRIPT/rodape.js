a743df56e5cadb515f89eed62665c5571b84c0c5
psilva88 <arthurpvp10sub0@gmail.com>
2026-04-09 10:27:50 -0300

    Delete Projeto2-JAVASCRIPT directory

--- Projeto2-JAVASCRIPT/rodape.js
+++
@@ -1,9 +0,0 @@
-function Rodape() {
-    return (
-        <footer className="rodape">
-            <h3>CosForge Crowdfunding</h3>
-            <p>Ajude a manter a comunidade Maker viva financiando os projetos em desenvolvimento.</p>
-            <button className="btn-crowdfunding">☕ Apoiar a Plataforma (R$ 5)</button>
-        </footer>
-    );
-}
